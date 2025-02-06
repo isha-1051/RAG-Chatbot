16cfa190250689f326f5281d05625296fcfecbe3
isha <isha@topsinfosolutions.com>
2025-02-06 18:03:52 +0530

    nl to sql with agents

--- app/api/query-sql/route.ts
+++ app/api/query-sql/route.ts
@@ -1,4 +1,3 @@
-// import mysql from "mysql2/promise";
 import { ChatOpenAI } from "@langchain/openai";
 import { SqlDatabase } from "langchain/sql_db";
 import { DataSource } from "typeorm";
@@ -6,41 +5,56 @@ import { Annotation, StateGraph } from "@langchain/langgraph";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { pull } from "langchain/hub";
 import { QuerySqlTool } from "langchain/tools/sql";
+import { SqlToolkit } from "langchain/agents/toolkits/sql";
 import { z } from "zod";
-// const dbConfig = process.env;
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { AIMessage, BaseMessage, isAIMessage } from "@langchain/core/messages";
+
+const prettyPrint = (message: BaseMessage) => {
+  let txt = `[${message._getType()}]: ${message.content}`;
+  if ((isAIMessage(message) && message.tool_calls?.length) || 0 > 0) {
+    const tool_calls = (message as AIMessage)?.tool_calls
+      ?.map((tc) => `- ${tc.name}(${JSON.stringify(tc.args)})`)
+      .join("\n");
+    txt += ` \nTools: \n${tool_calls}`;
+  }
+  console.log(txt);
+};
 
 const llm = new ChatOpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_AI_KEY,
   model: "gpt-4o-mini",
   temperature: 0
 });
 
-// const datasource = new DataSource({
-//     type: "mysql",
-//     host: process.env.DB_HOST || "localhost",
-//     // port: 3000,
-//     username: process.env.DB_USER || "root",
-//     password: process.env.DB_PASS || "tops12345",
-//     database: process.env.DB_NAME || "sql_prompt",
-// });
+const datasource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST || "localhost",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "tops12345",
+    database: process.env.DB_NAME || "sql_prompt",
+});
 
-// const db = await SqlDatabase.fromDataSourceParams({
-//     appDataSource: datasource,
-// });
+const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+});
+
+const toolkit = new SqlToolkit(db, llm);
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
   const userQuestion = url.searchParams.get("question");
-  console.log("url =>", userQuestion);
+  console.log("user question =>", userQuestion);
 
   if (!userQuestion) {
     return new Response(JSON.stringify({ error: "Question is invalid" }), { status: 400 });
   }
 
-//   const result = await llm.invoke(userQuestion);
-//   console.log("result =>", result);
+  // const result = await llm.invoke(userQuestion);
+  // console.log("result =>", result);
 
-const InputStateAnnotation = Annotation.Root({
+  /*
+  const InputStateAnnotation = Annotation.Root({
     question: Annotation<string>,
   });
 
@@ -75,7 +89,6 @@ const InputStateAnnotation = Annotation.Root({
     return { result: await executeQueryTool.invoke(state.query) };
   };
 
-
   const generateAnswer = async (state: typeof StateAnnotation.State) => {
     const promptValue =
       "Given the following user question, corresponding SQL query, " +
@@ -99,10 +112,54 @@ const InputStateAnnotation = Annotation.Root({
 
   const graph = graphBuilder.compile();
 
-  const input = { question: "How many Employees are there?" };
+  const input = { question: userQuestion ?? "How many Employees are there?" };
 
   const result = await graph.invoke(input);
   console.log("result =>", result);
+  */
+ 
+  /* The SqlToolkit includes tools that can:
+    1. Create and execute queries
+    2. Check query syntax
+    3. Retrieve table descriptions
+  */
+ 
+  const tools = toolkit.getTools();
+  // console.log(
+  //   tools.map((tool) => ({ name: tool.name, description: tool.description }))
+  // );
 
-  return Response.json({ message: result || "Hello from the SQL API" });
+  const systemPromptTemplate = await pull<ChatPromptTemplate>("langchain-ai/sql-agent-system-prompt");
+
+  const systemMessage = await systemPromptTemplate.format({ dialect: "mysql", top_k: 5 });
+
+  const agent = createReactAgent({
+    llm: llm,
+    tools: tools,
+    stateModifier: systemMessage,
+  });
+
+  const input2 = {
+    // messages: [{ role: "user", content: "Which country's customers spent the most?" }],
+    // messages: [{ role: "user", content: "Describe the orders table" }],
+    messages: [{ role: "user", content: userQuestion }],
+  };
+
+  const result3 = await agent.stream(input2, { streamMode: "values" });
+  // console.log("result3", result3[0]);
+
+  const array = [];
+  for await (const step of result3) {
+    const lastMessage = step.messages[step.messages.length - 1];
+    // console.log("lastMessage =>", lastMessage);
+    prettyPrint(lastMessage);
+    console.log("-----\n");
+
+    array.push(lastMessage.content);
+  }
+
+  // console.log("query =>", array[array.length - 3]);
+  // console.log("answer =>", array[array.length - 1]);
+  
+  return Response.json({ message: array[array.length - 1] || "Hello from the SQL API" });
 }

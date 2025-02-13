136b2a76d236338af4feb4261968b480e24be992
ankurchovatiya <ankurchovatiya@topsinfosolutions.com>
2025-02-13 14:50:56 +0530

    path changes

--- app/utils/gmail/auth.ts
+++ app/utils/gmail/auth.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const SCOPES = ["https://www.googleapis.com/auth/gmail.send"];
-const CREDENTIALS_PATH = "/home/tops/Desktop/Ankur/RAG/RAG-Chatbot/client_secret_428465629032-dr1cshqja5mfmltsio2epqi97a0k70bm.apps.googleusercontent.com.json"; // Path to your credentials file
+const CREDENTIALS_PATH = "/home/tops/Desktop/Ankur/RAG/RAG-Chatbot/secret/client_secret_428465629032-dr1cshqja5mfmltsio2epqi97a0k70bm.apps.googleusercontent.com.json"; // Path to your credentials file
 
 export async function authorize() {
   const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, "utf8"));
@@ -18,7 +18,7 @@ export async function authorize() {
   );
 
   // Load tokens if already generated
-  const TOKEN_PATH = "/home/tops/Desktop/Ankur/RAG/RAG-Chatbot/token.json";
+  const TOKEN_PATH = "/home/tops/Desktop/Ankur/RAG/RAG-Chatbot/secret/token.json";
   if (fs.existsSync(TOKEN_PATH)) {
     const token = JSON.parse(fs.readFileSync(TOKEN_PATH, "utf8"));
     oAuth2Client.setCredentials(token);
